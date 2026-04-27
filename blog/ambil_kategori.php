2d9ae58842626b02fa368b271c7e89f928e2645a
OCHIDEZ <zakyngawi13@gmail.com>
2026-04-27 15:10:32 +0700

    ambil_kategori.php

--- blog/ambil_kategori.php
+++ blog/ambil_kategori.php
@@ -6,12 +6,9 @@ $result = $conn->query($query);
 $data = [];
 
 while ($row = $result->fetch_assoc()) {
-    // Sanitasi output untuk mencegah serangan XSS
     $row['nama_kategori'] = htmlspecialchars($row['nama_kategori']);
     $row['keterangan'] = htmlspecialchars($row['keterangan']);
     $data[] = $row;
 }
-
-// Mengirimkan data dalam format JSON agar mudah dibaca oleh Javascript (Fetch API)
 echo json_encode($data);
 ?>
