995d512a0e56b1061f796ac12859df0a5c0fd8cd
OCHIDEZ <zakyngawi13@gmail.com>
2026-04-27 15:10:46 +0700

    ambil_penulis.php

--- blog/ambil_penulis.php
+++ blog/ambil_penulis.php
@@ -7,7 +7,6 @@ while ($row = $result->fetch_assoc()) {
     $row['nama_depan'] = htmlspecialchars($row['nama_depan']);
     $row['nama_belakang'] = htmlspecialchars($row['nama_belakang']);
     $row['user_name'] = htmlspecialchars($row['user_name']);
-    // Logika gambar default
     $row['foto_url'] = empty($row['foto']) ? 'uploads_penulis/default.png' : 'uploads_penulis/' . $row['foto'];
     $data[] = $row;
 }
