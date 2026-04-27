ab2af8a213795ed1d772a80bf4064cf26538bd62
OCHIDEZ <zakyngawi13@gmail.com>
2026-04-27 16:52:51 +0700

    Delete blog directory

--- blog/ambil_satu_penulis.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'koneksi.php';
-$id = $_GET['id'];
-$res = $conn->query("SELECT id, nama_depan, nama_belakang, user_name FROM penulis WHERE id = $id");
-echo json_encode($res->fetch_assoc());
-?>
